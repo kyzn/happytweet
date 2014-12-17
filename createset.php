54c62ae89f0d1a130aa67b7b7cb622b67b74c848
Kivanc Yazan <kivancyazan@gmail.com>
2014-12-17 03:25:51 +0200

    command line parameter passing for automated calls

--- createset.php
+++ createset.php
@@ -6,10 +6,8 @@ require_once("./twitter_config.php");
 require_once("connection.php");
 
 /*If wrong password, go back.*/
-if ($_GET["p"]!="lI73hdf82jA8f"){
-	echo "You're not allowed to do that!";
-}
-else{
+$pass="lI73hdf82jA8f";
+if($argv[1]==$pass || $_GET["p"]==$pass){
 
 	for($i=0;$i<10;$i++){
 
@@ -33,6 +31,8 @@ else{
 
 	header('Location: ./index.php');
 
+}else{
+	echo "You're not allowed to do that!";
 }
 
 ?>
--- twitter_reset_weekly.php
+++ twitter_reset_weekly.php
@@ -6,12 +6,14 @@ require_once("./twitter_config.php");
 require_once("connection.php");
 
 /*If wrong password, go back.*/
-if ($_GET["p"]!="lI73hdf82jA8f"){
-	echo "You're not allowed to do that!";
-}
-else{
+$pass="lI73hdf82jA8f";
+
+if($argv[1]==$pass || $_GET["p"]==$pass){
 	$stmt = $db->prepare("UPDATE Users SET WeeklyPoint=0;");
 	$stmt->execute();
 	echo "Done!";
+}else{
+	echo "You're not allowed to do that!";	
 }
+
 ?>
